e62c47f1129695745889755fcc037ad1bd9bfc1b
Necdet UYGUR <necdet.uygur@gmail.com>
2023-12-24 03:34:14 +0300

    Server

---
+++ nocket.server.js
@@ -0,0 +1,21 @@
+if (!(document.head.innerText.indexOf("nocket") > -1)) {
+  var script = document.createElement("script");
+  script.src = "https://necdetuygur.github.io/local-watch/nocket.js";
+  document.head.appendChild(script);
+}
+
+const send = (id) => {
+  if (!id) return;
+  const video =
+    window.frames[0].frameElement.contentWindow.document.querySelector(
+      "#player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video"
+    );
+  const data = {
+    time: video.currentTime,
+    play: video.paused ? 0 : 1,
+    speed: video.playbackRate,
+  };
+  Nocket.Send(id, data);
+};
+
+send(prompt("id", ""));
