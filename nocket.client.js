8f2f9348cc5e6e512d468c62d2f464bb10ecc06b
Necdet UYGUR <necdet.uygur@gmail.com>
2023-12-26 00:07:33 +0300

    Client

--- nocket.client.js
+++ nocket.client.js
@@ -1,21 +1,44 @@
+const CreateDenk = () => {
+  var div = document.createElement("div");
+  div.id = "denk";
+  document.body.appendChild(div);
+  window.denk = document.getElementById("denk");
+  denk.style.position = "fixed";
+  denk.style.left = 0;
+  denk.style.top = 0;
+  denk.style.zIndex = 99999;
+  denk.style.font = "23px consolas";
+  denk.style.color = "#fff";
+  denk.style.backgroundColor = "#000";
+  denk.style.padding = "5px";
+};
+
+const Denk = (message) => {
+  window.denk.innerHTML = `<input value="${Nocket.ID}" /><br>Denk: ${message}`;
+  setTimeout(() => {
+    window.denk.innerHTML = `<input value="${Nocket.ID}" /><br>Denk: Veri bekleniyor`;
+  }, 5e3);
+};
+
 if (!(document.head.innerText.indexOf("nocket.js") > -1)) {
   var script = document.createElement("script");
   script.src = "https://necdetuygur.github.io/local-watch/nocket.js";
   document.head.appendChild(script);
   /* */
   setTimeout(() => {
+    CreateDenk();
     Nocket.Listen((data) => {
+      Denk(`Yeni veri geldi: ${data.time}`);
+
       const video =
         window.frames[0].frameElement.contentWindow.document.querySelector(
           "#player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video"
         );
-      data.time = parseFloat(data.time);
-      data.play = data.play == 1;
-      data.speed = parseFloat(data.speed);
+      if (data.hasOwnProperty("play") && data.play) {
+        data.play == 1 ? video.play() : video.pause();
+      }
       video.currentTime = parseFloat(data.time);
-      data.play ? video.play() : video.pause();
       video.playbackRate = parseFloat(data.speed);
     });
-    prompt("", Nocket.ID);
-  }, 3e3);
+  }, 5e3);
 }
