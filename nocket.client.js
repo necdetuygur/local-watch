961f40b630468c3a0140af73c69ae70971f31fe0
Necdet UYGUR <necdet.uygur@gmail.com>
2023-12-26 00:24:06 +0300

    Client

--- nocket.client.js
+++ nocket.client.js
@@ -8,8 +8,8 @@ const CreateDenk = () => {
   denk.style.top = 0;
   denk.style.right = 0;
   denk.style.zIndex = 99999;
-  denk.style.fontFamily = "courier new";
-  denk.style.fontSize = "17px";
+  denk.style.fontFamily = "arial";
+  denk.style.fontSize = "13px";
   denk.style.color = "#fff";
   denk.style.backgroundColor = "#000";
   denk.style.padding = "5px";
